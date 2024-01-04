605489d183529b2ce41205a54f3199564008df01
aluz <aluzdev@gmail.com>
2024-01-03 22:40:07 -0600

    header done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,10 @@
+import Image from "next/image";
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <section className="flex place-content-between">
+      <Image src="kolorsbiz.svg" alt="kolorsbiz logo" width={120} height={40} />
+      <Image src={"burger.svg"} alt="burger menu" width={30} height={30} />
+    </section>
+  );
 };
