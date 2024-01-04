e0bf0964c85607cc2302eb5abbc5f0f8d2d2c32b
aluz <aluzdev@gmail.com>
2024-01-04 06:36:30 -0600

    header a little wonky

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,18 +1,24 @@
 import Image from "next/image";
-import Head from "next/head";
 
 export const Hero = () => {
   return (
-    <section className="flex h-[600px] w-full flex-col overflow-hidden rounded-lg bg-center">
-      <Image
-        src="/hero-bus.png"
-        alt="bus"
-        sizes="100vw"
-        width={350}
-        height={180}
-        layout="responsive"
-      />
-      <p>develop</p>
+    <section className="flex h-[70vh] w-full flex-col overflow-hidden rounded-lg bg-center">
+      <div className="relative h-[100%] w-full bg-center">
+        <Image src="/hero-bus.png" alt="bus" fill objectFit="cover" />
+      </div>
+      <div className="bg-grape text-centerleading-[45px] min-h-[400px] w-full pt-6">
+        <div className="flex flex-col items-center justify-center gap-6 text-center">
+          <p className="w-[300px] text-pretty text-2xl font-bold text-white">
+            El transporte de personal hecho a tu medida
+          </p>
+          <p className="w-60 text-pretty text-2xl leading-[37px] text-white">
+            Soluciones para las necesidades de tu empresa
+          </p>
+          <button className="bg-grass h-14 w-[210px] rounded-[40px] font-bold text-white">
+            Conoce más
+          </button>
+        </div>
+      </div>
     </section>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,19 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "~/styles/globals.css";
-import { Roboto } from "next/font/google";
+import { Poppins } from "next/font/google";
 
-const roboto = Roboto({
+const poppins = Poppins({
   weight: ["400", "700"],
   subsets: ["latin"],
 });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <div className=" flex min-h-screen flex-col place-items-center gap-8 bg-cream p-20 px-2.5">
+    <div className="flex min-h-screen flex-col place-items-center gap-8 bg-[#FAFAFF] bg-cream p-20 px-2.5">
       <style jsx global>{`
         html {
-          font-family: ${roboto.style.fontFamily};
+          font-family: ${poppins.style.fontFamily};
         }
       `}</style>
       <Component {...pageProps} />
