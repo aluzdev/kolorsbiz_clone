2f05e97ee911f779663c1c8b8a431bcb82dd7b91
aluzdev <aluzdev@gmail.com>
2024-01-04 23:59:19 -0800

    carousel functionality

--- src/components/Unique.tsx
+++ src/components/Unique.tsx
@@ -1,3 +1,24 @@
+import Carousel from "nuka-carousel";
+import React from "react";
+
 export const Unique = () => {
-  return <div>Unique</div>;
+  return (
+    <div>
+      <Carousel
+        autoplay
+        autoplayInterval={100}
+        defaultControlsConfig={{
+          nextButtonClassName: "hidden",
+          prevButtonClassName: "hidden",
+          pagingDotsContainerClassName: "gap-10 carousel-dots",
+          pagingDotsStyle: { fill: "#9873E6" },
+        }}
+      >
+        <img src="/burger.svg" />
+        <img src="/favicon.ico" />
+        <img src="/hero-bus.png" />
+        <img src="/kolorsbiz.svg" />
+      </Carousel>
+    </div>
+  );
 };
