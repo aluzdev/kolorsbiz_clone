db7bc784844a48323ffe5df5e8a9e85e5a4c9383
aluzdev <aluzdev@gmail.com>
2024-01-04 17:45:49 -0800

    fixing warnings (#5)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,9 @@ export const Header = () => {
   return (
     <section className="flex w-full flex-row place-content-between px-2">
       <Image src="kolorsbiz.svg" alt="kolorsbiz logo" width={120} height={40} />
-      <Image src={"burger.svg"} alt="burger menu" width={30} height={30} />
+      <div className="h-auto w-auto">
+        <Image src="burger.svg" alt="burger menu" width={30} height={30} />
+      </div>
     </section>
   );
 };
