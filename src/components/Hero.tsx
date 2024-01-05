14e7dd2c7175b48ca22d189e795e9e63ea0c620e
aluz <aluzdev@gmail.com>
2024-01-04 19:26:38 -0600

    font details

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,13 +17,13 @@ export const Hero = () => {
       />
       <div className="text-centerleading-[45px] min-h-[400px] w-full bg-grape pt-6">
         <div className="flex flex-col items-center justify-center gap-6 text-center">
-          <p className="w-[300px] text-pretty text-2xl font-bold text-white">
-            El transporte de personal hecho a tu medida
+          <p className="w-[300px] text-3xl font-bold text-white">
+            El transporte <br /> de personal <br /> hecho a tu medida
           </p>
           <p className="w-60 text-pretty text-2xl leading-[37px] text-white">
             Soluciones para las necesidades de tu empresa
           </p>
-          <button className="h-14 w-[210px] rounded-[40px] bg-grass font-bold text-white">
+          <button className="h-14 w-[210px]  rounded-[40px] bg-grass text-xl font-bold text-white">
             Conoce más
           </button>
         </div>
