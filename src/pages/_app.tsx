3397512fdc7441554bcc708a34c9b7e6cf57fe13
aluzdev <aluzdev@gmail.com>
2024-01-04 17:33:07 -0800

    feat: hero mobile
    * added roboto font
    
    * header a little wonky
    
    * image behaves better
    
    * hero mobile finished
    
    * font details
    
    * cursor pointer
    
    * deleted background style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "~/styles/globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <div className=" flex min-h-screen flex-col place-items-center gap-8 bg-cream p-20 px-2.5">
+    <div className="flex min-h-screen flex-col place-items-center gap-8 bg-[#FAFAFF] bg-cream p-20 px-2.5">
+      <style jsx global>{`
+        html {
+          font-family: ${poppins.style.fontFamily};
+        }
+      `}</style>
       <Component {...pageProps} />
     </div>
   );
