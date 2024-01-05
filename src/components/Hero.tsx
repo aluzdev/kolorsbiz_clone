4ddb97cb38538eb12df4c8d0455f0b0d86b89a1b
aluzdev <aluzdev@gmail.com>
2024-01-04 17:43:45 -0800

    feat: hero mobile
    * added roboto font
    
    * header a little wonky
    
    * image behaves better
    
    * hero mobile finished
    
    * font details
    
    * cursor pointer
    
    * deleted background style

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,32 @@
 import Image from "next/image";
+
 export const Hero = () => {
   return (
-    <section className="flex h-[600px] w-full flex-col overflow-hidden rounded-lg bg-center">
+    <section className="flex w-full flex-col overflow-hidden rounded-3xl bg-center">
       <Image
         src="/hero-bus.png"
         alt="bus"
-        sizes="100vw"
-        width={350}
+        width={360}
         height={180}
-        layout="responsive"
+        quality={100}
+        style={{
+          width: "100%",
+          objectFit: "cover",
+        }}
       />
-      <p>develop</p>
+      <div className="text-centerleading-[45px] min-h-[400px] w-full bg-grape pt-6">
+        <div className="flex flex-col items-center justify-center gap-6 text-center">
+          <p className="w-[300px] text-3xl font-bold text-white">
+            El transporte <br /> de personal <br /> hecho a tu medida
+          </p>
+          <p className="w-60 text-pretty text-2xl leading-[37px] text-white">
+            Soluciones para las necesidades de tu empresa
+          </p>
+          <button className="h-14 w-[210px] cursor-pointer  rounded-[40px] bg-grass text-xl font-bold text-white">
+            Conoce más
+          </button>
+        </div>
+      </div>
     </section>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,19 @@ import flowbite from "flowbite/plugin";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./app/**/*.{ts,tsx}", "./node_modules/flowbite-react/lib/**/*.js", "./src/**/*.tsx"],
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./node_modules/flowbite-react/lib/**/*.js",
+    "./src/**/*.tsx",
+  ],
   plugins: [flowbite],
+  theme: {
+    colors: {
+      creme: "#FFF5EA",
+      grape: "#9873E6",
+      grass: "#22CFAE",
+    },
+  },
 };
 
 export default config;
