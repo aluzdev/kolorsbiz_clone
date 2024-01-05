b37c9f46fcbab8e32576c6e566b56bd7ceb11878
aluz <aluzdev@gmail.com>
2024-01-04 18:59:48 -0600

    image behaves better

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,11 +2,15 @@ import Image from "next/image";
 
 export const Hero = () => {
   return (
-    <section className="flex h-[70vh] w-full flex-col overflow-hidden rounded-lg bg-center">
-      <div className="relative h-[100%] w-full bg-center">
-        <Image src="/hero-bus.png" alt="bus" fill objectFit="cover" />
-      </div>
-      <div className="bg-grape text-centerleading-[45px] min-h-[400px] w-full pt-6">
+    <section className="flex w-full flex-col overflow-hidden rounded-lg bg-center">
+      <Image
+        src="/hero-bus.png"
+        alt="bus"
+        width={360}
+        height={180}
+        style={{ width: "100%", objectFit: "cover" }}
+      />
+      <div className="text-centerleading-[45px] min-h-[400px] w-full bg-grape pt-6">
         <div className="flex flex-col items-center justify-center gap-6 text-center">
           <p className="w-[300px] text-pretty text-2xl font-bold text-white">
             El transporte de personal hecho a tu medida
@@ -14,7 +18,7 @@ export const Hero = () => {
           <p className="w-60 text-pretty text-2xl leading-[37px] text-white">
             Soluciones para las necesidades de tu empresa
           </p>
-          <button className="bg-grass h-14 w-[210px] rounded-[40px] font-bold text-white">
+          <button className="h-14 w-[210px] rounded-[40px] bg-grass font-bold text-white">
             Conoce más
           </button>
         </div>
