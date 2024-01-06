cf666f99a458d338d465c2c26260fea9ed0534f3
aluzdev <aluzdev@gmail.com>
2024-01-06 00:36:20 -0800

    cleaned code

--- src/components/Unique.tsx
+++ src/components/Unique.tsx
@@ -3,14 +3,47 @@ import React, { useEffect, useRef } from "react";
 import UniqueCard from "./UniqueCard";
 
 export const Unique = () => {
+  const uniqueCardsData = [
+    {
+      src: "/routes.svg",
+      title: "diseño de rutas",
+      description: "para garantizar la puntualidad",
+    },
+    {
+      src: "/points.svg",
+      title: "puntos de abordaje",
+      description: "seguros y flexibles",
+    },
+    {
+      src: "/security.svg",
+      title: "monitoreo",
+      description:
+        "en tiempo real para la seguridad y seguimiento de pasajeros",
+    },
+    {
+      src: "/driver.svg",
+      title: "conductores",
+      description: "capacitados",
+    },
+    {
+      src: "/soap.svg",
+      title: "medidas de sanidad",
+      description: "para tu protección",
+    },
+    {
+      src: "/headset.svg",
+      title: "atención",
+      description: "24/7",
+    },
+  ];
   return (
     <section className=" min-h-[430px]">
       <Carousel
         autoplay
         autoplayInterval={2500}
+        wrapAround
         className="max-w-64"
         cellAlign="center"
-        wrapAround
         defaultControlsConfig={{
           nextButtonClassName: "hidden",
           prevButtonClassName: "hidden",
@@ -18,32 +51,13 @@ export const Unique = () => {
           pagingDotsStyle: { fill: "#9873E6" },
         }}
       >
-        <UniqueCard
-          src="/routes.svg"
-          title="diseño de rutas"
-          description="para garantizar la puntualidad"
-        />
-        <UniqueCard
-          src="/points.svg"
-          title="puntos de abordaje"
-          description="seguros y flexibles"
-        />
-        <UniqueCard
-          src="/security.svg"
-          title="monitoreo"
-          description="en tiempo real para la seguridad y seguimiento de pasajeros"
-        />
-        <UniqueCard
-          src="/driver.svg"
-          title="conductores"
-          description="capacitados"
-        />
-        <UniqueCard
-          src="/soap.svg"
-          title="medidas de sanidad"
-          description="para tu protección"
-        />
-        <UniqueCard src="/headset.svg" title="atención" description="24/7" />
+        {uniqueCardsData.map(({ src, title, description }) => (
+          <UniqueCard
+            src={src}
+            title={title}
+            description={description}
+          ></UniqueCard>
+        ))}
       </Carousel>
     </section>
   );
